f58d7229da557c1fabebcb8389ecec0668a2cba8
dan1sh15 <danishqayyum9793@gmail.com>
2024-07-30 00:18:23 +0530

    added loader

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,10 +3,11 @@ import { NavLink, useNavigate } from 'react-router-dom';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import { AppContext } from '../context/AppContext';
 import toast from 'react-hot-toast';
+import Loader from './Loader';
 
 const Login = () => {
 
-    const { setLoading, setUser } = useContext(AppContext);
+    const { setLoading, setUser, loading } = useContext(AppContext);
     const [showPassword, setShowPassword] = useState(false);
     const [userData, setUserData] = useState({
         email: "",
@@ -53,54 +54,60 @@ const Login = () => {
     }
 
   return (
-    <div className='min-h-screen h-auto w-full flex justify-center items-center max-phone:p-7 max-[250px]:p-3 p-10'>
-      <div className='w-[40%] max-lg:w-[60%] max-md:w-[70%] max-sm:w-[90%] max-[400px]:w-full bg-white rounded-lg p-5 max-sm:p-3 max-sm:py-10 flex flex-col gap-y-8 max-phone:gap-y-5 py-14 shadow-lg' >
-        <h1 className='text-3xl max-md:text-2xl max-phone:text-xl font-semibold text-center'>Welcome to <span className='text-[#4B36CC]'>Workflo</span>!</h1>
-        <form onSubmit={submitHandler} className='w-full flex flex-col items-center gap-y-5 max-phone:gap-y-3'>
+    <>
+        {
+            loading ? (<Loader />) : (
+                <div className='min-h-screen h-auto w-full flex justify-center items-center max-phone:p-7 max-[250px]:p-3 p-10'>
+                    <div className='w-[40%] max-lg:w-[60%] max-md:w-[70%] max-sm:w-[90%] max-[400px]:w-full bg-white rounded-lg p-5 max-sm:p-3 max-sm:py-10 flex flex-col gap-y-8 max-phone:gap-y-5 py-14 shadow-lg' >
+                        <h1 className='text-3xl max-md:text-2xl max-phone:text-xl font-semibold text-center'>Welcome to <span className='text-[#4B36CC]'>Workflo</span>!</h1>
+                        <form onSubmit={submitHandler} className='w-full flex flex-col items-center gap-y-5 max-phone:gap-y-3'>
 
-            <input 
-                type="email" 
-                placeholder='Your email'
-                className='outline-none border-none bg-[#EBEBEB] w-[80%] rounded-md px-5 py-3 max-phone:text-sm max-phone:p-3 max-phone:w-[90%]'
-                id='email'
-                name='email'
-                value={userData.email}
-                onChange={changeHandler}
-                required
-            />
+                            <input 
+                                type="email" 
+                                placeholder='Your email'
+                                className='outline-none border-none bg-[#EBEBEB] w-[80%] rounded-md px-5 py-3 max-phone:text-sm max-phone:p-3 max-phone:w-[90%]'
+                                id='email'
+                                name='email'
+                                value={userData.email}
+                                onChange={changeHandler}
+                                required
+                            />
 
-            <div className='bg-[#EBEBEB] w-[80%] rounded-md gap-x-1 px-5 py-3 flex items-center max-phone:text-sm max-phone:p-3 max-phone:w-[90%]'>
-                <input 
-                    type={`${showPassword ? 'text' : 'password'}`}
-                    placeholder='Password'
-                    className='outline-none border-none w-full bg-transparent'
-                    id='password'
-                    name='password'
-                    value={userData.password}
-                    onChange={changeHandler}
-                    required
-                    minLength={8}
-                />
-                {
-                    showPassword ? (<FaEyeSlash onClick={() => {
-                        setShowPassword(false);
-                    }} className='text-[#999999] cursor-pointer' />) : (<FaEye onClick={() => {
-                        setShowPassword(true);
-                    }} className='text-[#999999] cursor-pointer' />)
-                }
-                
-            </div>
+                            <div className='bg-[#EBEBEB] w-[80%] rounded-md gap-x-1 px-5 py-3 flex items-center max-phone:text-sm max-phone:p-3 max-phone:w-[90%]'>
+                                <input 
+                                    type={`${showPassword ? 'text' : 'password'}`}
+                                    placeholder='Password'
+                                    className='outline-none border-none w-full bg-transparent'
+                                    id='password'
+                                    name='password'
+                                    value={userData.password}
+                                    onChange={changeHandler}
+                                    required
+                                    minLength={8}
+                                />
+                                {
+                                    showPassword ? (<FaEyeSlash onClick={() => {
+                                        setShowPassword(false);
+                                    }} className='text-[#999999] cursor-pointer' />) : (<FaEye onClick={() => {
+                                        setShowPassword(true);
+                                    }} className='text-[#999999] cursor-pointer' />)
+                                }
+                                
+                            </div>
 
-            <button className='bg-[#9C93D4] w-[80%] rounded-md text-white py-2 max-phone:text-sm max-phone:p-3 max-phone:w-[90%]'>
-                Log In 
-            </button>
+                            <button className='bg-[#9C93D4] w-[80%] rounded-md text-white py-2 max-phone:text-sm max-phone:p-3 max-phone:w-[90%]'>
+                                Log In 
+                            </button>
 
-            <div className='text-sm max-phone:text-xs text-center'>
-                Don't have an account? Create a <span className='text-blue-500'><NavLink to={'/signup'}>new account</NavLink></span>.
-            </div>
-        </form>
-      </div>
-    </div>
+                            <div className='text-sm max-phone:text-xs text-center'>
+                                Don't have an account? Create a <span className='text-blue-500'><NavLink to={'/signup'}>new account</NavLink></span>.
+                            </div>
+                        </form>
+                    </div>
+                    </div>
+            )
+        }
+    </>
   )
 }
 
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -3,10 +3,11 @@ import { NavLink, useNavigate } from 'react-router-dom';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import toast from 'react-hot-toast';
 import { AppContext } from '../context/AppContext';
+import Loader from './Loader';
 
 const Signup = () => {
 
-    const { setUser, setLoading } = useContext(AppContext);
+    const { setUser, setLoading, loading } = useContext(AppContext);
     const [showPassword, setShowPassword] = useState(false);
     const [userData, setUserData] = useState({
         name: "",
@@ -56,64 +57,70 @@ const Signup = () => {
     }
 
   return (
-    <div className='min-h-screen h-auto w-full flex justify-center items-center max-phone:p-7 max-[250px]:p-3 p-10'>
-      <div className='w-[40%] max-lg:w-[60%] max-md:w-[70%] max-sm:w-[90%] max-[400px]:w-full bg-white rounded-lg p-5 max-sm:p-3 max-sm:py-10 flex flex-col gap-y-8 max-phone:gap-y-5 py-14 shadow-lg' >
-        <h1 className='text-3xl max-md:text-2xl max-phone:text-xl font-semibold text-center'>Welcome to <span className='text-[#4B36CC]'>Workflo</span>!</h1>
-        <form onSubmit={submitHandler} className='w-full flex flex-col items-center gap-y-5 max-phone:gap-y-3'>
-            <input 
-                type="text" 
-                placeholder='Full name'
-                className='outline-none border-none bg-[#EBEBEB] w-[80%] rounded-md px-5 py-3 max-phone:text-sm max-phone:p-3 max-phone:w-[90%]'
-                id='name'
-                name='name'
-                value={userData.name}
-                onChange={changeHandler}
-                required
-            />
+    <>
+        {
+            loading ? (<Loader />) : (
+                <div className='min-h-screen h-auto w-full flex justify-center items-center max-phone:p-7 max-[250px]:p-3 p-10'>
+                    <div className='w-[40%] max-lg:w-[60%] max-md:w-[70%] max-sm:w-[90%] max-[400px]:w-full bg-white rounded-lg p-5 max-sm:p-3 max-sm:py-10 flex flex-col gap-y-8 max-phone:gap-y-5 py-14 shadow-lg' >
+                        <h1 className='text-3xl max-md:text-2xl max-phone:text-xl font-semibold text-center'>Welcome to <span className='text-[#4B36CC]'>Workflo</span>!</h1>
+                        <form onSubmit={submitHandler} className='w-full flex flex-col items-center gap-y-5 max-phone:gap-y-3'>
+                            <input 
+                                type="text" 
+                                placeholder='Full name'
+                                className='outline-none border-none bg-[#EBEBEB] w-[80%] rounded-md px-5 py-3 max-phone:text-sm max-phone:p-3 max-phone:w-[90%]'
+                                id='name'
+                                name='name'
+                                value={userData.name}
+                                onChange={changeHandler}
+                                required
+                            />
 
-            <input 
-                type="email" 
-                placeholder='Your email'
-                className='outline-none border-none bg-[#EBEBEB] w-[80%] rounded-md px-5 py-3 max-phone:text-sm max-phone:p-3 max-phone:w-[90%]'
-                id='email'
-                name='email'
-                value={userData.email}
-                onChange={changeHandler}
-                required
-            />
+                            <input 
+                                type="email" 
+                                placeholder='Your email'
+                                className='outline-none border-none bg-[#EBEBEB] w-[80%] rounded-md px-5 py-3 max-phone:text-sm max-phone:p-3 max-phone:w-[90%]'
+                                id='email'
+                                name='email'
+                                value={userData.email}
+                                onChange={changeHandler}
+                                required
+                            />
 
-            <div className='bg-[#EBEBEB] w-[80%] rounded-md gap-x-1 px-5 py-3 flex items-center max-phone:text-sm max-phone:p-3 max-phone:w-[90%]'>
-                <input 
-                    type={`${showPassword ? 'text' : 'password'}`}
-                    placeholder='Password'
-                    className='outline-none border-none w-full bg-transparent'
-                    id='password'
-                    name='password'
-                    value={userData.password}
-                    onChange={changeHandler}
-                    required
-                    minLength={8}
-                />
-                {
-                    showPassword ? (<FaEyeSlash onClick={() => {
-                        setShowPassword(false);
-                    }} className='text-[#999999] cursor-pointer' />) : (<FaEye onClick={() => {
-                        setShowPassword(true);
-                    }} className='text-[#999999] cursor-pointer' />)
-                }
-                
-            </div>
+                            <div className='bg-[#EBEBEB] w-[80%] rounded-md gap-x-1 px-5 py-3 flex items-center max-phone:text-sm max-phone:p-3 max-phone:w-[90%]'>
+                                <input 
+                                    type={`${showPassword ? 'text' : 'password'}`}
+                                    placeholder='Password'
+                                    className='outline-none border-none w-full bg-transparent'
+                                    id='password'
+                                    name='password'
+                                    value={userData.password}
+                                    onChange={changeHandler}
+                                    required
+                                    minLength={8}
+                                />
+                                {
+                                    showPassword ? (<FaEyeSlash onClick={() => {
+                                        setShowPassword(false);
+                                    }} className='text-[#999999] cursor-pointer' />) : (<FaEye onClick={() => {
+                                        setShowPassword(true);
+                                    }} className='text-[#999999] cursor-pointer' />)
+                                }
+                                
+                            </div>
 
-            <button className='bg-[#9C93D4] w-[80%] rounded-md text-white py-2 max-phone:text-sm max-phone:p-3 max-phone:w-[90%]'>
-                Sign Up 
-            </button>
+                            <button className='bg-[#9C93D4] w-[80%] rounded-md text-white py-2 max-phone:text-sm max-phone:p-3 max-phone:w-[90%]'>
+                                Sign Up 
+                            </button>
 
-            <div className='text-sm max-phone:text-xs text-center'>
-                Already have an account? <span className='text-blue-500'><NavLink to={'/login'}>Log in</NavLink></span>.
-            </div>
-        </form>
-      </div>
-    </div>
+                            <div className='text-sm max-phone:text-xs text-center'>
+                                Already have an account? <span className='text-blue-500'><NavLink to={'/login'}>Log in</NavLink></span>.
+                            </div>
+                        </form>
+                    </div>
+                    </div>
+            )
+        }
+    </>
   )
 }
 
